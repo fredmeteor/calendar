faeb338fdba288b1419b51d31c9af9781f995760
fredmeteor <fredpeter09899@gmail.com>
2023-12-15 15:56:37 +0300

    Added submit event to the CalendarEntry

--- src/store.js
+++ src/store.js
@@ -9,6 +9,13 @@ export const store = {
 return this.state.data.find((day) => day.active);
 },
 
+submitEvent(eventDetails) {
+const activeDay = this.getActiveDay();
+activeDay.events.push({ "details": eventDetails, "edit": false });
+
+
+},
+
 setActiveDay (dayId) {
     this.state.data.map((dayObj) => {
       dayObj.id === dayId ? dayObj.active = true : dayObj.active = false;
